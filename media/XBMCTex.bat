5a456832374e09f8f017b99cee92062d1a9b4a3e
shen.franklin <shen.franklin@eb879ec2-c127-0410-ab2e-4128cf563e3a>
2007-02-28 15:17:42 +0000

    Fixed Build.bat to create correct Textures.xpr

--- media/XBMCTex.bat
+++ media/XBMCTex.bat
@@ -1,4 +1,3 @@
 @echo off
 
-START /B /WAIT media/XBMCTex -input media\Default\ -output media\textures.xpr -quality max -noprotect
-START /B /WAIT media/XBMCTex -input media\White\ -output media\White.xpr -quality max -noprotect
+START /B /WAIT media/XBMCTex -input media\ -output media\textures.xpr -quality max -noprotect
